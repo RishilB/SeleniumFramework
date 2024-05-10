884a976c9c29a2b7698580ffb96048289b0eac51
Rishil <risshilbhatt@gmail.com>
2024-05-10 23:32:05 +0530

    Initial Commit

---
+++ src/main/java/com/rbhatt/selenium/App.java
@@ -0,0 +1,13 @@
+package com.rbhatt.selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
