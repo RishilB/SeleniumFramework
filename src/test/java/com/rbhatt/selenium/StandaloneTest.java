d247a95146b03d750ed2a1d878b6e0eb81b323a9
Rishil <risshilbhatt@gmail.com>
2024-05-11 11:58:29 +0530

    StandaloneTest commit

---
+++ src/test/java/com/rbhatt/selenium/StandaloneTest.java
@@ -0,0 +1,17 @@
+package com.rbhatt.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class StandaloneTest {
+    public static void main(String[] args){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.get("https://rahulshettyacademy.com/client");
+    }
+}
