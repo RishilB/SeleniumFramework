a6e71980f4654b66ac210d9a98264b3692af87bc
RishilB <risshilbhatt@gmail.com>
2024-05-23 22:08:04 +0530

    Successfully Integrated generating Allure Results from Mvn test

--- src/test/java/cucumber/TestNGTestRunner.java
+++ src/test/java/cucumber/TestNGTestRunner.java
@@ -3,13 +3,16 @@ package cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
 
 
+@Test
 @CucumberOptions(
 		features = "src/test/java/cucumber",
 		glue = "com.rbhatt.selenium.StepDefinitions",
 		monochrome = true,
 		tags = "@Regression",
+		publish = true,
 		plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"})
 
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
