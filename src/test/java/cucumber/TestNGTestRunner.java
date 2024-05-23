517a129802ae2b9b62b1c96bcccdc108996eeb36
RishilB <risshilbhatt@gmail.com>
2024-05-23 09:10:18 +0530

    SF_01 Initial Commit about integrating Allure Report

--- src/test/java/cucumber/TestNGTestRunner.java
+++ src/test/java/cucumber/TestNGTestRunner.java
@@ -3,10 +3,11 @@ package cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/cucumber",
+@CucumberOptions(
+		features = "src/test/java/cucumber",
 		glue = "com.rbhatt.selenium.StepDefinitions",
 		monochrome = true,
 		tags = "@Regression",
-		plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"})
+		plugin = {"pretty","io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 }
