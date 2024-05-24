8f42abeb587b859e416b1cd318f00b2d0f816eba
Rishil Bhatt <risshilbhatt@gmail.com>
2024-05-24 17:27:19 +0530

    Cucumber Retry working with Allure Reports

--- src/test/java/cucumber/TestNGTestRunner.java
+++ src/test/java/cucumber/TestNGTestRunner.java
@@ -4,7 +4,6 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.Listeners;
 
-//@Listeners({com.rbhatt.selenium.TestComponents.Retry.class})
 @CucumberOptions(features = "src/test/java/cucumber",
 		glue = "com.rbhatt.selenium.StepDefinitions",
 		monochrome = true,
