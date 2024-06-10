49fe796710c2db0d1f1bfeda09c6cb0a7539a392
Rishil Bhatt <risshilbhatt@gmail.com>
2024-06-10 18:58:47 +0530

    Added example for File Upload using AutoIT

---
+++ src/test/java/com/rbhatt/selenium/TestComponents/FileUpload.java
@@ -0,0 +1,17 @@
+package com.rbhatt.selenium.TestComponents;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class FileUpload {
+	public static void main(String[] args){
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://altoconvertpdftojpg.com/");
+		driver.findElement(By.id("browse")).click();
+	}
+}
