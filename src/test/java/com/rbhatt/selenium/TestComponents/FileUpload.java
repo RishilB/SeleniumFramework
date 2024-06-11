a4b64b737bc6a8edcc7c53ca9da0599fae98eae8
Rishil Bhatt <risshilbhatt@gmail.com>
2024-06-11 11:50:28 +0530

    file_upload.exe

--- src/test/java/com/rbhatt/selenium/TestComponents/FileUpload.java
+++ src/test/java/com/rbhatt/selenium/TestComponents/FileUpload.java
@@ -4,14 +4,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.IOException;
 import java.time.Duration;
 
 public class FileUpload {
-	public static void main(String[] args){
+	public static void main(String[] args) throws InterruptedException, IOException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://altoconvertpdftojpg.com/");
 		driver.findElement(By.id("browse")).click();
+		Thread.sleep(3000);
+		Runtime.getRuntime().exec(System.getProperty("user.dir") + "file_upload.exe");
+		
 	}
 }
